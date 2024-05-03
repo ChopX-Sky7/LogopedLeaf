2e5d95a9e71f98ce32968ecc64e0bff88da050b7
Sky SeVen <shrikelionel0@gmail.com>
2024-05-03 16:55:52 +0300

    Initial commit

---
+++ src/main/java/com/MVP/LogopedLeafApplication.java
@@ -0,0 +1,13 @@
+package com.MVP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogopedLeafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogopedLeafApplication.class, args);
+	}
+
+}
